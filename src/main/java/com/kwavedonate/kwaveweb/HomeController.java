8b9b5d28b0c98133a674af37fb84c2bfc6a7e744
kimdahye <dahye4321@naver.com>
2017-02-25 19:30:46 +0900

    no message

--- src/main/java/com/kwavedonate/kwaveweb/HomeController.java
+++ src/main/java/com/kwavedonate/kwaveweb/HomeController.java
@@ -10,19 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.social.connect.Connection;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.FacebookProfile;
-import org.springframework.social.facebook.api.UserOperations;
-import org.springframework.social.facebook.api.impl.FacebookTemplate;
-import org.springframework.social.facebook.connect.FacebookConnectionFactory;
-import org.springframework.social.oauth2.AccessGrant;
-import org.springframework.social.oauth2.GrantType;
-import org.springframework.social.oauth2.OAuth2Operations;
-import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.kwavedonate.kwaveweb.campaign.service.CampaignService;
 import com.kwavedonate.kwaveweb.core.util.BcryptEncoder;
 
-/**
+/** 
  * Handles requests for the application home page.
  */
 @Controller
@@ -44,42 +32,6 @@ public class HomeController {
 	@Resource(name="campaignService")
 	private CampaignService campaignService;
 	
-	@Autowired
-	private FacebookConnectionFactory connectionFactory;
-	
-	@Autowired
-	private OAuth2Parameters oAuth2Parameters;
-	
-	public String getAuthorizeUrl(){
-		OAuth2Operations oauthOperations = connectionFactory.getOAuthOperations();
-		String authorizeUrl = oauthOperations.buildAuthenticateUrl(GrantType.AUTHORIZATION_CODE, oAuth2Parameters);
-		return authorizeUrl;
-	}
-	
-	@Value("${facebook.redirect.url}")
-	private String redirectUrl;
-	
-	public Map<String, Object> getFacebookUserInfo(String code){
-		Map<String, Object> map = new HashMap<String, Object>();
-		OAuth2Operations oauthOperations = connectionFactory.getOAuthOperations();
-		
-		AccessGrant accessGrant = oauthOperations.exchangeForAccess(code, redirectUrl, null);
-		String accessToken = accessGrant.getAccessToken();
-		
-		Connection<Facebook> connection = connectionFactory.createConnection(accessGrant);
-		Facebook facebook = connection ==null? new FacebookTemplate(accessToken) : connection.getApi();
-		
-		UserOperations userOperations = facebook.userOperations();
-		FacebookProfile facebookProfile = null;
-		try{
-			facebookProfile = userOperations.getUserProfile();
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		map.put("facebookid", facebookProfile.getId());
-		return map;
-	}
 	
 	/**
 	 * 메인화면
